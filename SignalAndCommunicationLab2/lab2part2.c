9934ef67aab91159b5e0567443b31c8e74af1ce2
Alex <aej800@gmail.com>
2025-02-14 12:43:36 -0900

    finished the thing

--- SignalAndCommunicationLab2/lab2part2.c
+++ SignalAndCommunicationLab2/lab2part2.c
@@ -1,54 +1,66 @@
+// lab2part2.c
+// Alex Johnson
+// 2/14/2025
+// for cs321 lab 2 
+
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<sys/wait.h>
 
-void sigHandler (int);
+void sigReporter(int);
+void sigHandler(int);
 
 int main()
 { 
 
-	signal(SIGUSR1, sigHandler);
-	signal(SIGUSR2, sigHandler);
-	
-
-  int	pid = fork();
-	
+  int	pid = fork();	
+  signal(SIGINT, sigHandler);	
 	if(pid == 0) 
 	{
+    printf("Forked %d, into child %d\n", getppid(), getpid());
+    int ppid = getppid(); 
 		while(1)
 		{
 			int r = (rand() % 6) + 1; 
 			if((r%2 == 0))
 			{
-				raise(SIGUSR1);
+          kill(ppid, SIGUSR1);
 			}
 			else
 			{ 
-				raise(SIGUSR2);
+        kill(ppid, SIGUSR2);
 			} 	
 			sleep(r);  	
 		}
-	}  
-	//wait(NULL);
-  int status;
-  waitpid(pid, &status, 0);
-  if ( WIFEXITED(status) ){
-    int exit_status = WEXITSTATUS(status); //exit_status variable store the
-    printf("Exit status of the child was %d\n", exit_status);
-  }
-	
-} 
-
-
-void sigHandler(int sigNum)
-{ 
-	if(sigNum == SIGUSR1)
-	{
-		printf("SIGUSR1 recieved\n");
 	}
-	else if(sigNum == SIGUSR2)
-	{ 
-		printf("SIGUSR2 recieved\n");
-	}  
-} 
+  else if(pid < 0) 
+  {
+    printf("Fork() failed to create a thread\n");
+    exit(1);
+  } 
+  else
+  {
+	  signal(SIGUSR1, sigReporter);
+	  signal(SIGUSR2, sigReporter);
+	  wait(NULL);
+  } 
+}
+void sigReporter(int sigNum)
+{ 
+  printf("Recieved signal %d, ", sigNum);
+  if(sigNum == SIGUSR1) 
+  { 
+    printf("SIGUSR1\n");
+  } 
+  else if(sigNum == SIGUSR2)
+  {
+    printf("SIGUSR2\n");
+  } 
+}
+void sigHandler(int sigNum)
+{
+  printf("Exitting process %d, child of %d\n", getpid(),getppid());
+  exit(0);
+
+}
